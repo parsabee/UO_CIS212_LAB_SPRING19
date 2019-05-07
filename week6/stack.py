c0c0768cf5b7e7b3163fda9c21a34a4a11fd2bd5
Adam N <a1noack@csbsju.edu>
2019-05-06 17:21:03 -0700

    add check not empty to peek

--- week6/stack.py
+++ week6/stack.py
@@ -1,13 +1,18 @@
 class Stack():
 	def __init__(self):
 		self.l = []
+		
 	def push(self, element):
 		self.l.append(element)
+
 	def pop(self):
 		element = None
 		if len(self.l) > 0:
 			element = self.l[-1]
 			del self.l[-1]
 		return element
+
 	def peek(self):
-		return self.l[-1]
+		if len(self.l) > 0:
+			return self.l[-1]
+		return None
